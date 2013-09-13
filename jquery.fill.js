84ad35a012a6719e2d7558c982dfa908d6ad6202
simonfan <simonxd@gmail.com>
2013-09-13 13:07:17 -0300

    added support for attribute data mappping

--- jquery.fill.js
+++ jquery.fill.js
@@ -6,11 +6,11 @@
 \* ------------------------------------------------ */
 define(['jquery','underscore'], function($, undef) {
 
-	var fillers = {
-		'default': function($el, value) {
+	var tagFillers = {
+		default: function($el, value) {
 			return $el.html(value);
 		},
-		'INPUT': function($el, value) {
+		INPUT: function($el, value) {
 			/**
 			 * intercept only filling for checkboxes and radios
 			 * as the default jquery .val() method sets the checkboxes and radio input
@@ -24,17 +24,41 @@ define(['jquery','underscore'], function($, undef) {
 			
 			return $el.val(value);
 		},
-		'SELECT': function($el, value) {
+		SELECT: function($el, value) {
 			return $el.val(value);
 		},
-		'IMG': function($el, value) {
+		IMG: function($el, value) {
 			// trigger a change event when changing the image src
 			return $el.prop('src', value).trigger('change', value);
 		},
-		'A': function($el, value) {
+		A: function($el, value) {
 			return $el.prop('href', value)
-		}
+		},
 	};
+
+	/**
+	 * Fills an attribute from the $el.
+	 */
+	function fillAttribute($el, attributeSelector, value) {
+		var split = attributeSelector.split(':'),
+			method = split[0],
+			attr = split[1];
+
+		if (method === 'style' || method === 'css') {
+
+			$el.css(attr, value);
+
+		} else if (method === 'attr') {
+
+			$el.attr(attr, value);
+
+		} else if (method === 'prop') {
+
+			$el.prop(attr, value);
+
+		}
+	}
+
 
 	$.fn.fill = function (valueMap) {
 		var _this = this;
@@ -53,11 +77,37 @@ define(['jquery','underscore'], function($, undef) {
 
 
 			_.each(valueMap, function(value, selector) {
-				var $el = _this.find(selector),
-					tag = $el.prop('tagName'),
-					filler = fillers[ tag ] || fillers['default'];
 
-				filler($el, value);
+				/**
+				 * Try spliting the selector
+				 */
+				var split = _.map(selector.split('->'), function(str) {
+					return str.replace(/^\s+|\s+$/g,'');
+				});
+
+
+				if (split.length > 1) {
+					/**
+					 * Split is successful, try to get the attribute filler
+					 */
+					var selector = split[0],
+						// string that defines which attribute to be filled
+						attributeSelector = split[1],
+						$el = _this.find(selector);
+
+					fillAttribute($el, attributeSelector, value);
+
+
+				} else {
+					/**
+					 * Split unsuccessful, 
+					 */
+					var $el = _this.find(selector),
+						tag = $el.prop('tagName'),
+						filler = tagFillers[ tag ] || tagFillers['default'];
+
+					filler($el, value);
+				}
 			})
 
 		} else {
--- tests/demo.js
+++ tests/demo.js
@@ -1,9 +1,11 @@
 define(['jquery.fill'], function(undef) {
 	$('body').fill({
 		'#display-div': 'New text',
+		'#display-div -> attr:data-status': 'filled',
 		'select': 'oranges',
 
 		'.test-image': 'warning.gif',
+		'.test-image -> style:width': 100,
 		'.link': 'http://www.google.com',
 
 		'input[name="animals"]': ['lion','wolf'],
